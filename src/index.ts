875cdb3feac9df34a8ccd920ac45619da5d8094e
luimu64 <luimu@luimu.dev>
2024-05-14 19:17:17 +0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import * as cheerio from "cheerio";
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+const fetchNewStrip = async () => {
+	const fetchRes = await fetch(
+		"https://www.hs.fi/fingerpori/",
+		{
+			headers: {
+				"User-Agent":
+					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3",
+			},
+		}
+	);
+
+	const body = await fetchRes.text();
+	const $ = cheerio.load(body);
+
+	const stripContainer = $("figure.cartoon.image")[0];
+	const stripImageRaw = $(stripContainer).children("img").attr("data-srcset")?.replace(" 1920w", '')
+
+	return `https:${stripImageRaw}`
+}
+
+const message = async (url: string) => {
+	await fetch(WEBHOOK_URL,
+		{
+			method: 'POST',
+			headers:
+			{
+				'method': 'POST',
+				'content-type': 'application/json'
+			},
+			body: JSON.stringify({ content: url })
+		})
+}
+
+const doTasks = async () => {
+	const strip = await fetchNewStrip()
+	await message(strip)
+	return new Response(null, { status: 204 })
+}
+
+addEventListener('scheduled', event => {
+	event.waitUntil(doTasks())
+})
