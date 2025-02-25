19f33e0634c1b0da3a72d5dec15b36dfb98646f5
luimu64 <luimu@luimu.dev>
2025-02-25 15:20:21 +0200

    updated webhook to use hs api instead of scraping

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,45 @@
-import * as cheerio from "cheerio";
-
 const fetchNewStrip = async () => {
 	const fetchRes = await fetch(
-		"https://www.hs.fi/fingerpori/",
+		"https://www.hs.fi/api/laneitems/39221/list/normal/290",
 		{
 			headers: {
 				"User-Agent":
 					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.3",
 			},
-		}
+		},
+	).then((res) => res.text());
+
+	const imageData = JSON.parse(fetchRes);
+
+	const firstImageUrl = imageData[0].picture.url.replace(
+		"WIDTH.EXT",
+		"1920.avif",
 	);
 
-	const body = await fetchRes.text();
-	const $ = cheerio.load(body);
+	if (!firstImageUrl) {
+		throw new Error("Failed to fetch image URL");
+	}
 
-	const stripContainer = $("figure.cartoon.image")[0];
-	const stripImageRaw = $(stripContainer).children("img").attr("data-srcset")?.replace(" 1920w", '')
-
-	return `https:${stripImageRaw}`
-}
+	return firstImageUrl;
+};
 
 const message = async (url: string) => {
-	await fetch(WEBHOOK_URL,
-		{
-			method: 'POST',
-			headers:
-			{
-				'method': 'POST',
-				'content-type': 'application/json'
-			},
-			body: JSON.stringify({ content: url })
-		})
-}
+	await fetch(WEBHOOK_URL, {
+		method: "POST",
+		headers: {
+			method: "POST",
+			"content-type": "application/json",
+		},
+		body: JSON.stringify({ content: url }),
+	});
+};
 
 const doTasks = async () => {
-	const strip = await fetchNewStrip()
-	await message(strip)
-	return new Response(null, { status: 204 })
-}
+	const strip = await fetchNewStrip();
+	await message(strip);
+	return new Response(null, { status: 204 });
+};
 
-addEventListener('scheduled', event => {
-	event.waitUntil(doTasks())
-})
+addEventListener("scheduled", (event) => {
+	event.waitUntil(doTasks());
+});
